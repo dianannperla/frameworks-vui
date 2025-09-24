3ffda7898e324371897a3144d63931e66cdd6e65
dianannperla <waafirmelyza@hotmail.com>
2025-09-24 15:10:36 +0100

    Fixed greeting message to use English.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Chào mừng đến với Frameworks Vui!');
+  res.send('Welcome to Fun Frameworks!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server đang chạy trên http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
